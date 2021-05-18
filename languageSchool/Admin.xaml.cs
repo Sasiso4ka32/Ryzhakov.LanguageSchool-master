73ddef883b9a8614fd5c3baf3b2006795eb15b7d
Роман Рыжаков <ryzhackov.rom@gmail.com>
2021-05-18 20:36:42 +0300

    Задания 2го дня

---
+++ languageSchool/Admin.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace languageSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        List<Service> ServiswList = ClassBase.EM.Service.ToList();
+        public Admin()
+        {
+            InitializeComponent();
+            DGServises.ItemsSource = ServiswList;
+        }
+        int i = -1;
+        private void MediaElement_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                i++;
+                MediaElement ME = (MediaElement)sender;
+                Service S = ServiswList[i];
+                Uri U = new Uri(S.MainImagePath, UriKind.RelativeOrAbsolute);
+                ME.Source = U;
+                //   i++;
+            }
+        }
+
+        private void TextBlock_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                TextBlock TB = (TextBlock)sender;
+                Service S = ServiswList[i];
+                TB.Text = S.Title;
+                //  i++;
+            }
+
+        }
+
+        private void BRed_Initialized(object sender, EventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            if (BtnRed != null)
+            {
+                BtnRed.Uid = Convert.ToString(i);
+            }
+        }
+
+        private void BRed_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            int ind = Convert.ToInt32(BtnRed.Uid);
+            Service S = ServiswList[ind];
+            MessageBox.Show(S.Title);
+
+        }
+
+        private void StackPanel_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                StackPanel SP = (StackPanel)sender;
+                Service S = ServiswList[i];
+                if (S.Discount != 0)
+                {
+                    SP.Background = new SolidColorBrush(Color.FromRgb(231, 250, 191));
+                }
+            }
+        }
+
+        private void TextBlock_priceSkidos_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                TextBlock TB = (TextBlock)sender;
+                Service S = ServiswList[i];
+                TB.Text = Convert.ToInt32(S.Cost) + " ";
+                if (S.Discount> 0)
+                {
+                    TB.TextDecorations = TextDecorations.Strikethrough;
+                }
+                else
+                {
+                    TB.Visibility = Visibility.Collapsed;
+                }
+
+                //  i++;
+            }
+        }
+        private void TextBlock_price_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                TextBlock TB = (TextBlock)sender;
+                Service S = ServiswList[i];
+                if (S.Discount>0)
+                {
+                    TB.Text = Convert.ToInt32(S.Cost) * (1 - S.Discount) + " рублей ";
+                }
+                else
+                {
+                    TB.Text = Convert.ToInt32(S.Cost) + " рублей ";
+                }
+                
+                //  i++;
+            }
+        }
+        
+        private void TextBlock_time_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {
+                TextBlock TB = (TextBlock)sender;
+                Service S = ServiswList[i];
+                TB.Text = "за " + Convert.ToInt32(S.DurationInSeconds) /60 + " минут";
+                //  i++;
+            }
+        }
+        private void TextBlock_Skidos_Initialized(object sender, EventArgs e)
+        {
+            if (i < ServiswList.Count)
+            {                
+                TextBlock TB = (TextBlock)sender;
+                Service S = ServiswList[i];
+                if (S.Discount != 0)
+                {
+                    TB.Text = "* скидка " + Convert.ToDouble(S.Discount) * 100 + " %";
+                }                    
+                //  i++;
+            }
+        }
+
+        private void Bdel_Initialized(object sender, EventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            if (BtnRed != null)
+            {
+                BtnRed.Uid = Convert.ToString(i);
+            }
+
+        }
+        private void Bdel_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            int ind = Convert.ToInt32(BtnRed.Uid);
+            Service S = ServiswList[ind];
+            MessageBox.Show(S.Title);
+
+        }
+
+        private void Bnew_Initialized(object sender, EventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            if (BtnRed != null)
+            {
+                BtnRed.Uid = Convert.ToString(i);
+            }
+        }
+        private void Bnew_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnRed = (Button)sender;
+            int ind = Convert.ToInt32(BtnRed.Uid);
+            Service S = ServiswList[ind];
+            MessageBox.Show(S.Title);
+
+        }
+    }
+}
